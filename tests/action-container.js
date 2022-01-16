374b84417d34c7e2735129df3dbd9c387ed48420
Giuseppe De Palma <dpl.gsp@tutanota.com>
2022-01-16 22:02:10 +0100

    Add action container class and fix docker usage through child process and start porting withContainer

--- tests/action-container.js
+++ tests/action-container.js
@@ -1,15 +1,32 @@
-const fs = require("fs");
-var util = require('util')
-var { exec } = require('child_process');
+const existsSync = require("fs").existsSync;
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const platform = require("os").platform;
+
+/**
+ * For testing convenience, this interface abstracts away the REST calls to a
+ * container as blocking method calls of this interface.
+ */
+class ActionContainer {
+
+	constructor(init, run, runMultiple) {
+		this.init = init;
+		this.run = run;
+		this.runMultiple = runMultiple;
+	}
+	//   run: (value) => (Int, Option[JsObject]);
+	//   runMultiple: (values)(implicit ec: ExecutionContext) => Seq[(Int, Option[JsObject])];
+}
+
 
 // This fails if the docker binary couldn't be located.
 function dockerBin() {
 	const usrbindocker = "/usr/bin/docker";
-	let found = fs.existsSync(usrbindocker)
+	let found = existsSync(usrbindocker)
 	if (found) return usrbindocker;
 
 	const usrlocalbindocker = "/usr/local/bin/docker";
-	found = fs.existsSync(usrlocalbindocker)
+	found = existsSync(usrlocalbindocker)
 	if (found) return usrlocalbindocker;
 	return null;
 }
@@ -39,34 +56,34 @@ async function dockerCmd() {
 
 
 	// Test here that this actually works, otherwise throw a somewhat understandable error message
-	await proc(`${dockerCmdString} info`, 600, (code, stdout, stderr) => {
-		if (code && code != 0) {
-			throw `
-				|Unable to connect to docker host using ${dockerCmdString} as command string.
-				|The docker host is determined using the environment variable 'DOCKER_HOST'. 
-				|Please verify that it is set for your build/test process.`;
-		}
-		// else ok, do nothing
-	});
+	const code = await proc(`${dockerCmdString} info`, 1000).then(_res => 0, _err => 1);
+	if (code != 0) {
+		throw `
+		// Unable to connect to docker host using ${dockerCmdString} as command string.
+		// The docker host is determined using the environment variable 'DOCKER_HOST'. 
+		// Please verify that it is set for your build/test process.
+		// `;
+	}
 
 	return dockerCmdString;
 }
 
-function docker(command) {
-	return `${dockerCmd} ${command}`;
+async function docker(command) {
+	return `${await dockerCmd()} ${command}`;
 }
 // Tying it all together, we have a method that runs docker, waits for
 // completion for some time then returns the exit code, the output stream
 // and the error stream.
 async function awaitDocker(cmd, msDuration = 60000) {
-	let cp = await proc(docker(cmd), msDuration, (c, so, se) => {});
-	return { dode : cp.code, stdout: cp.stdout, stderr: cp.stderr };
+	let dockerCmd = await docker(cmd);
+	const out = await proc(dockerCmd, msDuration).then(res => { return { code: 0, ...res } }, err => { return { code: 1, err: err.message } });
+	return out;
 }
 
 // Runs a process asynchronously. Returns a future with (exitCode,stdout,stderr)
 function proc(cmd, msDuration, callback) {
-	return new timeoutPromise(msDuration, new Promise((resolve, reject) => {
-		return resolve(exec(cmd, callback));
+	return new timeoutPromise(msDuration, new Promise(async (resolve, reject) => {
+		return exec(cmd).then(res => resolve(res), err => reject(err));
 	}));
 }
 
@@ -88,25 +105,58 @@ function timeoutPromise(ms, promise) {
 	})
 }
 
+async function withContainer(imageName, environment = null) {
+	let rand = Math.random();
+	let name = imageName.toLowerCase().replaceAll(/[^a-z]/g, '') + rand
+
+	let envArgs = environment ? Object.entries(environment).map(([k, v]) => `-e ${k}=${v}`).join(' ') : "";
+
+	// We create the container... and find out its IP address...
+	let createContainer = (portFwd) => {
+		portFwd = portFwd ? `-p ${portFwd}:8080` : p = "";
+		let { code } = awaitDocker(`run ${portFwd} --name ${name} ${envArgs} -d ${imageName}`, 60000)
+		if (code != 0) {
+			// TODO stop here
+			// assert(runOut._1 == 0, "'docker run' did not exit with 0: " + runOut)
+		}
+	}
+
+	// ...find out its IP address...
+	let ip = 'localhost';
+	let p = 8988;
+	const timer = ms => new Promise(res => setTimeout(res, ms));
+	const osName = platform();
+	if (osName.includes('mac') && !process.env.DOCKER_HOST) { // on MacOSX, where docker for mac does not permit communicating with container directly
+		createContainer(p);// port must be available or docker run will fail
+		await timer(1500); // let container/server come up cleanly
+
+	} else {
+		// not "mac" i.e., docker-for-mac, use direct container IP directly (this is OK for Ubuntu, and docker-machine)
+		createContainer()
+		await timer(1500); // let container/server come up cleanly
+		let { code, stdout, err } = await awaitDocker(`inspect --format '{{.NetworkSettings.IPAddress}}' ${name}`, 10000)
+		if (code != 0) {
+			// TODO stop here 
+			// assert(ipOut._1 == 0, "'docker inspect did not exit with 0")
+		}
+		ip = stdout.replaceAll(/[^0-9.]/g, "");
+		p = 8080;
+	}
+
+	// ...we create an instance of the mock container interface...
+	const mock = new ActionContainer(
+		(value) => syncPost(ip, port, "/init", value),
+		(value) => syncPost(ip, port, "/run", value),
+		(values) => concurrentSyncPost(ip, port, "/run", values)
+	);
+
+	
+	return { ip, p };
+}
 
 async function main() {
-	const res = await awaitDocker("info", 1000);
-	console.log(res.code);
+	let code = await withContainer("openwhisk/action-nodejs-v14");
+	console.log(code);
 }
 
 main();
-
-// function withContainer(imageName, environment = null) {
-// 	return (code) => {
-// 		let rand = Math.random();
-// 		let name = imageName.toLowerCase().replaceAll(/[^a-z]/, '') + rand
-
-// 		let envArgs = Object.entries(data).map(([k, v]) => `-e ${k}=${v}`).join(' ');
-
-// 		// We create the container... and find out its IP address...
-// 		let createContainer = (portFwd) => {
-// 			let runOut = awaitDocker(`run ${portFwd.map(p => `-p ${p}:8080`).getOrElse("")} --name $name $envArgs -d $imageName`, 60000)
-// 			assert(runOut._1 == 0, "'docker run' did not exit with 0: " + runOut)
-// 		}
-// 	}
-// }
--- tests/nodejs.test.js
+++ tests/nodejs.test.js
@@ -1,19 +1,4 @@
-/**
- * For testing convenience, this interface abstracts away the REST calls to a
- * container as blocking method calls of this interface.
- */
-class ActionContainer {
-
-	constructor(init, run, runMultiple) {
-		this.init = init;
-		this.run = run;
-		this.runMultiple = runMultiple;
-	}
-	//   run: (value) => (Int, Option[JsObject]);
-	//   runMultiple: (values)(implicit ec: ExecutionContext) => Seq[(Int, Option[JsObject])];
-}
-
-const isBase64Pattern = new Regex('^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$');
+const isBase64Pattern = new RegExp('^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$');
 
 const TestConfig = {
 	code: "",
@@ -58,6 +43,9 @@ const initPayload = (code, main = "main", env = null) => {
 	}
 }
 
+// let nodejsContainerImageName = "openwhisk/action-nodejs-v14"
+// let nodejsTestDockerImageName = "nodejs14docker"
+
 class NodeJsActionContainerTests {
 	constructor(nodejsContainerImageName, nodejsTestDockerImageName, isTypeScript = false)
 
@@ -65,59 +53,6 @@ class NodeJsActionContainerTests {
 		withContainer(this.nodejsContainerImageName, env)(code)
 	}
 }
-
-
-
-
-
-// // We create the container... and find out its IP address...
-// def createContainer(portFwd: Option[Int] = None): Unit = {
-//   val runOut = awaitDocker(
-//     s"run ${portFwd.map(p => s"-p $p:8080").getOrElse("")} --name $name $envArgs -d $imageName",
-//     60.seconds)
-//   assert(runOut._1 == 0, "'docker run' did not exit with 0: " + runOut)
-// }
-
-// // ...find out its IP address...
-// val (ip, port) =
-//   if (System.getProperty("os.name").toLowerCase().contains("mac") && !sys.env
-//         .get("DOCKER_HOST")
-//         .exists(_.trim.nonEmpty)) {
-//     // on MacOSX, where docker for mac does not permit communicating with container directly
-//     val p = 8988 // port must be available or docker run will fail
-//     createContainer(Some(p))
-//     Thread.sleep(1500) // let container/server come up cleanly
-//     ("localhost", p)
-//   } else {
-//     // not "mac" i.e., docker-for-mac, use direct container IP directly (this is OK for Ubuntu, and docker-machine)
-//     createContainer()
-//     val ipOut = awaitDocker(s"""inspect --format '{{.NetworkSettings.IPAddress}}' $name""", 10.seconds)
-//     assert(ipOut._1 == 0, "'docker inspect did not exit with 0")
-//     (ipOut._2.replaceAll("""[^0-9.]""", ""), 8080)
-//   }
-
-// // ...we create an instance of the mock container interface...
-// val mock = new ActionContainer {
-//   def init(value: JsValue): (Int, Option[JsObject]) = syncPost(ip, port, "/init", value)
-//   def run(value: JsValue): (Int, Option[JsObject]) = syncPost(ip, port, "/run", value)
-//   def runMultiple(values: Seq[JsValue])(implicit ec: ExecutionContext): Seq[(Int, Option[JsObject])] =
-//     concurrentSyncPost(ip, port, "/run", values)
-// }
-
-// try {
-//   // ...and finally run the code with it.
-//   code(mock)
-//   // I'm told this is good for the logs.
-//   Thread.sleep(100)
-//   val (_, out, err) = awaitDocker(s"logs $name", 10.seconds)
-//   (out, err)
-// } finally {
-//   awaitDocker(s"kill $name", 10.seconds)
-//   awaitDocker(s"rm $name", 10.seconds)
-// }
-// }
-
-
 
 describe("Nodejs v14 Runtime", () => {
 	it("should handle initialization with no code", () => {
