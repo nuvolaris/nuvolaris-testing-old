4e0bcb1d99161ebbf7efdc4cf734c688553bec0b
Giuseppe De Palma <dpl.gsp@tutanota.com>
2022-01-22 20:15:55 +0100

    Add eslint to keep style consistent and fix linting problems

--- tests/action-container.js
+++ tests/action-container.js
@@ -1,136 +1,169 @@
-const existsSync = require("fs").existsSync;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+const existsSync = require('fs').existsSync;
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
-const platform = require("os").platform;
+const platform = require('os').platform;
 
 
 const setupContainer = async (imageName, environment = null) => {
-	// Prepare the data for the container
-	let containerName = buildContainerName(imageName);
-	let envArgs = extractEnvArgs(environment);
+  // Prepare the data for the container
+  const containerName = buildContainerName(imageName);
+  const envArgs = extractEnvArgs(environment);
 
-	// Create the container and get its IP address
-	let { ip, port } = await runContainer(imageName, containerName, envArgs);
-	return { containerName, ip, port };
-}
+  // Create the container and get its IP address
+  const {ip, port} = await runContainer(imageName, containerName, envArgs);
+  return {containerName, ip, port};
+};
 
 const runCodeInContainer = async (code, name) => {
-	let result = {};
-	try {
-		await code();
-		// I'm told this is good for the logs.
-		await timer(100);
-		let { stdout, err } = await execDockerCmd(`logs ${name}`);
-		result = { out: stdout, err }
-	} catch {
-		return { err: "error while running code in container." };
-	}
+  let result = {};
+  try {
+    await code();
+    // I'm told this is good for the logs.
+    await timer(100);
+    const {stdout, err} = await execDocker(`logs ${name}`);
+    result = {out: stdout, err};
+  } catch {
+    return {err: 'error while running code in container.'};
+  }
 
-	return result;
-}
+  return result;
+};
 
 const tearDownContainer = async (name) => {
-	await execDockerCmd(`kill ${name}`);
-	await execDockerCmd(`rm ${name}`);
-}
+  await execDocker(`kill ${name}`);
+  await execDocker(`rm ${name}`);
+};
 
 const runContainer = async (imageName, contName, envArgs) => {
-	const createContainer = async (portFwd) => {
-		portFwd = portFwd ? `-p ${portFwd}:8080` : portFwd = "";
-		let { code } = await execDockerCmd(`run ${portFwd} --name ${contName} ${envArgs} -d ${imageName}`)
-		if (code != 0) {
-			// TODO stop here
-			// assert(runOut._1 == 0, "'docker run' did not exit with 0: " + runOut)
-		}
-		await timer(500); // let container/server come up cleanly
-	}
+  const createContainer = async (portFwd) => {
+    portFwd = portFwd ? `-p ${portFwd}:8080` : portFwd = '';
 
-	let ip = 'localhost';
-	let port = 8988;
-	if (isDockerForMac()) {// on MacOSX, where docker for mac does not permit communicating with container directly
-		await createContainer(port);// port must be available or docker run will fail
-	} else {
-		// not "mac" i.e., docker-for-mac, use direct container IP directly (this is OK for Ubuntu, and docker-machine)
-		await createContainer()
-		let { code, stdout } = await execDockerCmd(`inspect --format '{{.NetworkSettings.IPAddress}}' ${contName}`)
-		if (code != 0) {
-			// TODO stop here 
-			// assert(ipOut._1 == 0, "'docker inspect did not exit with 0")
-		}
-		ip = stdout.replaceAll(/[^0-9.]/g, "");
-		port = 8080;
-	}
-	return { ip, port };
-}
+    const cmd = `run ${portFwd} --name ${contName} ${envArgs} -d ${imageName}`;
+    const {code} = await execDocker(cmd);
+    if (code != 0) {
+      // TODO stop here
+      // assert(runOut._1 == 0, "'docker run' did not exit with 0: " + runOut)
+    }
+    await timer(500); // let container/server come up cleanly
+  };
 
-const timer = ms => new Promise(res => setTimeout(res, ms));
+  let ip = 'localhost';
+  let port = 8988;
+  // on MacOSX docker for mac doesnt allow direct communication with container
+  if (isDockerForMac()) {
+    await createContainer(port);// port must be available or run will fail
+  } else {
+    // not "mac" do use container IP directly (OK for Ubuntu and docker-machine)
+    await createContainer();
+    const cmd = `inspect --format '{{.NetworkSettings.IPAddress}}' ${contName}`;
+    const {code, stdout} = await execDocker(cmd);
+    if (code != 0) {
+      // TODO stop here
+      // assert(ipOut._1 == 0, "'docker inspect did not exit with 0")
+    }
+    ip = stdout.replaceAll(/[^0-9.]/g, '');
+    port = 8080;
+  }
+  return {ip, port};
+};
 
-const buildContainerName = (imageName) => imageName.toLowerCase().replaceAll(/[^a-z]/g, '') + Math.random();
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-const extractEnvArgs = (e) => e ? Object.entries(e).map(([k, v]) => `-e ${k}=${v}`).join(' ') : "";
+const buildContainerName = (imageName) =>
+  imageName.toLowerCase().replaceAll(/[^a-z]/g, '') + Math.random();
 
-const isDockerForMac = () => platform().toLowerCase().includes('mac') && !process.env.DOCKER_HOST;
+const extractEnvArgs = (e) =>
+  e ? Object.entries(e).map(([k, v]) => `-e ${k}=${v}`).join(' ') : '';
+
+const isDockerForMac = () =>
+  platform().toLowerCase().includes('mac') && !process.env.DOCKER_HOST;
 
 // Tying it all together, we have a method that runs docker, waits for
 // completion for some time then returns the exit code, the output stream
 // and the error stream.
-const execDockerCmd = async (cmd) => {
-	let dockerCmd = dockerCmdString(cmd);
-	const out = await exec(dockerCmd).then(res => { return { code: 0, ...res } }, err => { return { code: 1, err: err.message } });
-	return out;
-}
+const execDocker = async (cmd) => {
+  const dockerCmd = dockerCmdString(cmd);
+  const out = await exec(dockerCmd).then((res) => {
+    return {code: 0, ...res};
+  }, (err) => {
+    return {code: 1, err: err.message};
+  });
+  return out;
+};
 
 const dockerCmdString = (command) => `${dockerCmd()} ${command}`;
 
 
-let dockerCmdEval = "";
+let dockerCmdEval = '';
 const dockerCmd = () => {
+  // lazy eval of function
+  if (dockerCmdEval) {
+    return dockerCmdEval;
+  }
 
-	// lazy eval of function
-	if (dockerCmdEval) {
-		return dockerCmdEval;
-	}
+  let dockerHost = process.env.DOCKER_HOST || '';
+  // if (!dockerHost) {} TODO try to get it from a whisk.properties file?
 
-	let dockerHost = process.env.DOCKER_HOST || "";
-	// if (!dockerHost) {} TODO try to get it from a whisk.properties file?
+  if (dockerHost) {
+    dockerHost = ` --host ${dockerHost}`;
+  }
 
-	if (dockerHost) {
-		dockerHost = ` --host ${dockerHost}`;
-	}
+  const fullDockerString = dockerBin() + dockerHost;
 
-	let fullDockerString = dockerBin() + dockerHost;
-
-	// Test here that this actually works, otherwise throw a somewhat understandable error message
-	exec(`${fullDockerString} info`).catch(_err => {
-		throw `
+  // Test here that this actually works, else throw an error message
+  exec(`${fullDockerString} info`).catch((_err) => {
+    throw new Error(`
 Unable to connect to docker host using ${fullDockerString} as command string.
 The docker host is determined using the environment variable 'DOCKER_HOST'. 
 Please verify that it is set for your build/test process.\n
-`;
-	});
+`);
+  });
 
-	dockerCmdEval = fullDockerString;
-	return fullDockerString;
-}
+  dockerCmdEval = fullDockerString;
+  return fullDockerString;
+};
 
-// This fails if the docker binary couldn't be located.
+/**
+ * Check that docker is available on the machine and get its exec path
+ * This fails if the docker binary couldn't be located.
+ * @return {string} docker command string
+ */
 function dockerBin() {
-	const usrbindocker = "/usr/bin/docker";
-	let found = existsSync(usrbindocker)
-	if (found) {
-		return usrbindocker;
-	}
-	const usrlocalbindocker = "/usr/local/bin/docker";
-	found = existsSync(usrlocalbindocker)
-	if (found) {
-		return usrlocalbindocker;
-	}
-	return "";
+  const usrbindocker = '/usr/bin/docker';
+  let found = existsSync(usrbindocker);
+  if (found) {
+    return usrbindocker;
+  }
+  const usrlocalbindocker = '/usr/local/bin/docker';
+  found = existsSync(usrlocalbindocker);
+  if (found) {
+    return usrlocalbindocker;
+  }
+  return '';
 }
 
 
 module.exports = {
-	setupContainer,
-	runCodeInContainer,
-	tearDownContainer
-}
+  setupContainer,
+  runCodeInContainer,
+  tearDownContainer,
+};
--- tests/nodejs.test.js
+++ tests/nodejs.test.js
@@ -1,81 +1,126 @@
-const setupContainer = require("./action-container").setupContainer;
-const runCodeInContainer = require("./action-container").runCodeInContainer;
-const tearDownContainer = require("./action-container").tearDownContainer;
-const mockContainerApi = require("./container-api").mockContainerApi;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+const setupContainer = require('./action-container').setupContainer;
+const runCodeInContainer = require('./action-container').runCodeInContainer;
+const tearDownContainer = require('./action-container').tearDownContainer;
+const mockContainerApi = require('./container-api').mockContainerApi;
 
+// eslint-disable-next-line max-len
 const isBase64Pattern = new RegExp('^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)$');
 
-function initPayload(code, main = "main", env = null) {
-	let b = false;
-	if (code) {
-		let t = code.trim();
-		b = (t.length > 0) && (t.length % 4 == 0) && isBase64Pattern.test(t);
-	}
+/**
+ * Creates a payload for the initialization of a container
+ * @param {function} code code to run in a container
+ * @param {string} main name of the main function
+ * @param {object} env env variables
+ * @return {object} payload with code, main function and env vars
+ */
+function initPayload(code, main = 'main', env = null) {
+  let b = false;
+  if (code) {
+    const t = code.trim();
+    b = (t.length > 0) && (t.length % 4 == 0) && isBase64Pattern.test(t);
+  }
 
-	return {
-		value: {
-			code,
-			main,
-			binary: b,
-			env
-		}
-	}
+  return {
+    value: {
+      code,
+      main,
+      binary: b,
+      env,
+    },
+  };
 }
 
-describe("Nodejs v14 Runtime", () => {
-	const nodejs14Image = "openwhisk/action-nodejs-v14";
-	let containerData;
-	let api;
+describe('Nodejs v14 Runtime', () => {
+  const nodejs14Image = 'openwhisk/action-nodejs-v14';
+  let containerData;
+  let api;
 
-	async function runWithActionContainer(code) {
-		await runCodeInContainer(code, containerData.name);
-	}
+  /**
+   * Runs code function in container
+   * @param {function} code to run in container
+   * @return {object} result of the code
+   */
+  async function runWithActionContainer(code) {
+    return await runCodeInContainer(code, containerData.name);
+  }
 
-	beforeAll(async () => {
-		containerData = await setupContainer(nodejs14Image);
-		api = mockContainerApi(containerData.ip, containerData.port);
-	});
+  beforeAll(async () => {
+    containerData = await setupContainer(nodejs14Image);
+    api = mockContainerApi(containerData.ip, containerData.port);
+  });
 
-	afterAll(() => {
-		let name = containerData.containerName;
-		return tearDownContainer(name);
-	});
+  afterAll(() => {
+    const name = containerData.containerName;
+    return tearDownContainer(name);
+  });
 
-	it("should handle initialization with no code", async () => {
-		const code = async () => {
-			let { status } = await api.init(initPayload("", ""));
-			expect(status).not.toBe(200);
-		};
+  it('should handle initialization with no code', async () => {
+    const code = async () => {
+      const {status} = await api.init(initPayload('', ''));
+      expect(status).not.toBe(200);
+    };
 
-		await runWithActionContainer(code);
-	})
+    await runWithActionContainer(code);
+  });
 
-	it("should handle initialization with no content", async () => {
+  it('should handle initialization with no content', async () => {
+    const code = async () => {
+      const {status, data} = await api.init({});
+      expect(status).not.toBe(200);
+      expect(data).toStrictEqual({error: 'Missing main/no code to execute.'});
+    };
 
-		const code = async () => {
-			let { status, data } = await api.init({});
-			expect(status).not.toBe(200);
-			expect(data).toStrictEqual({ error: "Missing main/no code to execute." });
-			// JsObject("error" -> JsString("The action failed to generate or locate a binary. See logs for details."))))
-		};
+    await runWithActionContainer(code);
+  });
 
-		await runWithActionContainer(code);
-	})
+  it('should run and report an error for not returning a json object', () => {
 
-	// it should "run and report an error for function not returning a json object" in {
+  });
 
-	// it should "fail to initialize a second time" in {
+  it('should fail to initialize a second time', () => {
 
-	// it should "invoke non-standard entry point" in {
+  });
 
-	// it should "echo arguments and print message to stdout/stderr" in {
+  it('should invoke non-standard entry point', () => {
 
-	// it should "handle unicode in source, input params, logs, and result" in {
+  });
 
-	// it should "export environment variables before initialization" in {
+  it('should echo arguments and print message to stdout/stderr', () => {
 
-	// it should "confirm expected environment variables" in {
+  });
 
-	// it should "echo a large input" 
+  it('should handle unicode in source, input params, logs, and result', () => {
 
-})
+  });
+
+  it('should export environment variables before initialization', () => {
+
+  });
+
+  it('should confirm expected environment variables', () => {
+
+  });
+
+  it('should echo a large input', () => {
+
+  });
+});
